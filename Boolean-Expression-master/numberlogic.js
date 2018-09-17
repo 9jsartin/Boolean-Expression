deb04d5f31aba5339984baaf3bb43f7a14a1f408
9jsartin <42806630+9jsartin@users.noreply.github.com>
2018-09-17 13:54:11 -0500

    Updated File

---
+++ Boolean-Expression-master/numberlogic.js
@@ -0,0 +1,56 @@
+//Create three numbers x, y, and z.  Set them equal to some random number of your choosing.
+
+let x=7;
+let y=5;
+let z=2;
+
+/*
+*
+*  Create a series of numeric expressions which evaluate to TRUE or FALSE.
+*
+*/
+
+//just using x
+let positiveX = (x > 0);
+let negativeX = (x < 0);
+
+//using all 3 variables
+let allPositive= (x > 0 ) && (y > 0) && (z > 0);
+let allNegative= ();
+
+//using all 3 variables
+let atLeastOnePositive;
+let atLeastOneNegative;
+
+
+/*
+*
+*  This section of code below here shows the product of your work.
+*
+*/
+
+function showResults(){
+    const intro =
+    `The variable x = ${x}
+    The variable y = ${y}
+    The variable z = ${z}`;
+
+    const posneg = 
+    `positive x : ${positiveX}
+    negative x : ${negativeX}`;
+
+    const allornothing =
+    `All the variables are positive ${allPositive}
+    All the variables are negative ${allNegative}`;
+
+    const atLeastOne =
+        `At least one variable is positive: ${ atLeastOnePositive}
+        At least one variable is negative: ${ atLeastOneNegative}`;
+
+    console.log(intro);
+    console.log(posneg);
+    console.log(allornothing);
+    console.log(atLeastOne)
+    
+}
+showResults()
